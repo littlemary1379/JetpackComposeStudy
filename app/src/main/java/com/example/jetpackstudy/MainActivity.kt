5e8d1f66e41de3f71189f8a498e443952d5e12d0
mary <littlemary1379@gmail.com>
2023-12-14 17:57:35 +0900

    add compose Modifier

--- app/src/main/java/com/example/jetpackstudy/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.jetpackstudy
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -32,8 +35,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             //Test()
             MaterialTheme {
-                Surface {
-                    MessageCard(msg = Message(author = "test author", content = "test content"))
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(msg = Message(author = "real author", content = "real content"))
                 }
             }
         }
@@ -45,9 +48,21 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    @Preview
+    @Preview(name = "Light Mode",
+        showBackground = true,
+        uiMode = Configuration.UI_MODE_NIGHT_NO
+    )
+    @Preview(
+        name = "Dark Mode",
+        showBackground = true,
+        uiMode = Configuration.UI_MODE_NIGHT_YES
+    )
     fun PreviewMessage() {
-        MessageCard(msg = Message(author = "test author", content = "test content"))
+        MaterialTheme {
+            Surface {
+                MessageCard(msg = Message(author = "test author", content = "test content"))
+            }
+        }
     }
 
     @Composable
@@ -60,14 +75,31 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
                     .size(Dp(40f))
                     .clip(CircleShape)
+                    .border(
+                        width = Dp(1.5f),
+                        color = MaterialTheme.colorScheme.primary,
+                        CircleShape
+                    )
             )
 
             Spacer(modifier = Modifier.width(Dp(8.0f)))
 
             Column {
-                Text(text = msg.author)
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
                 Spacer(modifier = Modifier.height(Dp(4.0f)))
-                Text(text = msg.content)
+
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = Dp(1f)) {
+                    Text(
+                        text = msg.content,
+                        modifier = Modifier.padding(Dp(4f)),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
             }
 
         }
