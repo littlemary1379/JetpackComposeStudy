72962bb0b057509236fb0ec0b41ebd1046dbcf50
mary <littlemary1379@gmail.com>
2023-12-18 11:16:34 +0900

    add Row and Button

--- app/src/main/java/com/example/jetpackstudy/CodeLabActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/CodeLabActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.jetpackstudy.theme.JetPackStudyTheme
 
 class CodeLabActivity : ComponentActivity() {
@@ -33,17 +38,29 @@ class CodeLabActivity : ComponentActivity() {
 
     }
 
+//    @Composable
+//    private fun MyApp(modifier: Modifier = Modifier) {
+//        Surface(
+//            modifier = modifier,
+//            color = MaterialTheme.colorScheme.background
+//        ) {
+//            Greeting(name = "Android")
+//        }
+//    }
+
     @Composable
-    private fun MyApp(modifier: Modifier = Modifier) {
-        Surface(
-            modifier = modifier,
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Greeting(name = "Android")
+    fun MyApp(
+        modifier: Modifier = Modifier,
+        names: List<String> = listOf("World", "Compose")
+    ) {
+        Column(modifier = modifier.padding(vertical = Dp(4f))) {
+            for (name in names) {
+                Greeting(name = name)
+            }
         }
     }
 
-    @Preview(showBackground = true, name = "Text Preview")
+    @Preview(showBackground = true, name = "Text Preview", widthDp = 320)
     @Composable
     private fun DefaultPreview() {
         JetPackStudyTheme {
@@ -52,12 +69,21 @@ class CodeLabActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun Greeting(name : String) {
-        Surface(color = MaterialTheme.colorScheme.primary) {
-            //Text(text = "Hello $name", modifier = Modifier.padding(Dp(24f)))
-            Column(modifier = Modifier.padding(Dp(24f))) {
-                Text(text = "Hello.")
-                Text(text = name)
+    private fun Greeting(name: String) {
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "Hello, ")
+                    Text(text = name)
+                }
+
+                ElevatedButton(onClick = {}) {
+                    Text(text = "Show More")
+                }
+
             }
         }
     }
